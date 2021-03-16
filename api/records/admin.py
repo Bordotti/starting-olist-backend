f1246eace41698c7b601b55c45b67ae2e58009f2
luciheit@gmail.com <luciheit@gmail.com>
2021-03-16 10:33:10 -0300

    creating model and admin Call Records

--- api/records/admin.py
+++ api/records/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CallRecord
 
-# Register your models here.
+
+class CallRecordAdmin(admin.ModelAdmin):
+    list_display = ('type_call', 'timestamp', 'call_id', 'source', 'destination')
+
+
+admin.site.register(CallRecord, CallRecordAdmin)
--- api/records/models.py
+++ api/records/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class CallRecord(models.Model):
+
+    type_choice = (('START', 'start at'), ('END', 'end at'))
+
+    type_call = models.CharField(max_length=50, choices=type_choice)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    call_id = models.CharField(max_length=64)
+    source = models.CharField(max_length=13)
+    destination = models.CharField(max_length=13)
+
+    class Meta:
+        unique_together = ('call_id', 'type_call')
+
+
+# class StartCallRecorder(CallRecord):
+#     def __init__(self, type_call, timestamp, call_id, source, destination):
+#         super().__init__(type_call, timestamp, call_id)
+#         self.source = models.CharField(max_length=13)
+#         self.destination = models.CharField(max_length=13)
+
+# class EndCallRecorder(CallRecord):
+#     def __init__(self, type_call, timestamp, call_id)
+
