ec328b6ab05574f68855a2264b84517715e9bcf0
luciheit@gmail.com <luciheit@gmail.com>
2021-03-22 22:32:05 -0300

    creating duration serializer and Bill class

--- api/records/serializers.py
+++ api/records/serializers.py
@@ -1,8 +1,11 @@
-from .models import CallRecord
+from .models import CallRecord, Bill
 from rest_framework import serializers
 
 
 class CallRecordSerializer(serializers.ModelSerializer):
+
+    duration = serializers.SerializerMethodField()
+
     class Meta:
         model = CallRecord
         fields = (
@@ -10,4 +13,29 @@ class CallRecordSerializer(serializers.ModelSerializer):
             'initial_call',
             'end_call',
             'source',
-            'destination')
+            'destination',
+            'duration'
+        )
+
+    def get_duration(self, obj):
+        duration = obj.end_call - obj.initial_call
+        return duration.total_seconds()
+
+
+class BillSerializer(serializers.ModelSerializer):
+
+    duration = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Bill
+        fields = (
+            'id',
+            'source',
+            # 'initial_call',
+            # 'duration'
+            # 'price'
+        )
+
+    # def get_duration(self, obj):
+    #     duration = obj.end_call - obj.initial_call
+    #     return duration.total_seconds()
