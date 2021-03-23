1bceedac8dae774e45eecdb352a4be3b88fed7ce
luciheit@gmail.com <luciheit@gmail.com>
2021-03-22 22:33:07 -0300

    creating Bill views and url

--- api/records/views.py
+++ api/records/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets, status
-from .serializers import CallRecordSerializer
-from .models import CallRecord
+from .serializers import CallRecordSerializer, BillSerializer
+from .models import CallRecord, Bill
 from rest_framework.response import Response
 
 
 class CallRecordViewSet(viewsets.ModelViewSet):
     queryset = CallRecord.objects.all()
     serializer_class = CallRecordSerializer
+
+
+class BillViewSet(viewsets.ModelViewSet):
+    queryset = Bill.objects.all()
+    serializer_class = BillSerializer
